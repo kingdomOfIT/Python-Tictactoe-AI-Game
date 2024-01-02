1d351c3cfaaa982ee9ef714eae377553352ddfe7
Amir Kahriman <amirkahriman@Amirs-MacBook-Air.local>
2024-01-02 21:22:16 +0100

    style: add screen and background style

--- tictactoe.py
+++ tictactoe.py
@@ -1,12 +1,46 @@
 import sys
 import pygame
 from constans import *
+import numpy as np
 
+#PYGAME SETUP
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("TIC TAC TOE AI")
+screen.fill(BG_COLOR)
+
+class Board: 
+
+    def __init__(self):
+        self.squares = np.zeros( (ROWS, COLS) )
+
+    def mark_sqr(self, row, col, player):
+        pass
+
+class Game: 
+
+    def __init__(self):
+        self.board = Board()
+        self.show_lines()
+
+    def show_lines(self):
+        # vertical
+        pygame.draw.line(screen, LINE_COLOR, (SQSIZE, 0), (SQSIZE, HEIGHT), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (WIDTH - SQSIZE, 0), (WIDTH - SQSIZE, HEIGHT), LINE_WIDTH)
+        #horizontal
+        pygame.draw.line(screen, LINE_COLOR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT-SQSIZE), (WIDTH, HEIGHT-SQSIZE), LINE_WIDTH)
 
 def main():
-    pass
+
+    game = Game()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        pygame.display.update()
 
 main()
