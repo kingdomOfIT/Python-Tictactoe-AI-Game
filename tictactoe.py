f4c03ea8df42fce45f5d0f9ed83744fc765ea8bd
Amir Kahriman <amirkahriman@Amirs-MacBook-Air.local>
2024-01-02 21:03:29 +0100

    Project init

---
+++ tictactoe.py
@@ -0,0 +1,12 @@
+import sys
+import pygame
+from constans import *
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("TIC TAC TOE AI")
+
+def main():
+    pass
+
+main()
