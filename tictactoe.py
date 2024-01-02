686ed42819d1b7616a65cc64a6447695eb9c528f
Amir Kahriman <amirkahriman@Amirs-MacBook-Air.local>
2024-01-02 21:39:59 +0100

    feat: add console board logic

--- tictactoe.py
+++ tictactoe.py
@@ -15,12 +15,16 @@ class Board:
         self.squares = np.zeros( (ROWS, COLS) )
 
     def mark_sqr(self, row, col, player):
-        pass
+        self.squares[row][col] = player
 
+    def empty_sqr(self, row, col):
+        return self.squares[row][col] == 0
+    
 class Game: 
 
     def __init__(self):
         self.board = Board()
+        self.player = 1
         self.show_lines()
 
     def show_lines(self):
@@ -31,15 +35,32 @@ class Game:
         pygame.draw.line(screen, LINE_COLOR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
         pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT-SQSIZE), (WIDTH, HEIGHT-SQSIZE), LINE_WIDTH)
 
+    def next_turn(self):
+        self.player = self.player % 2 + 1
+
 def main():
 
     game = Game()
+    board = game.board
 
+    # This is mainloop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = event.pos
+                row = pos[1] // SQSIZE
+                col = pos[0] // SQSIZE
+
+                if board.empty_sqr(row, col):
+                    board.mark_sqr(row, col, game.player)
+                    game.draw_fig(row, col)
+                    game.next_turn()
+
+                    # print(board.squares)
         
         pygame.display.update()
 
